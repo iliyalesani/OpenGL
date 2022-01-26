28cfc2d7a369234b594ae3dfe7adbf8168d75102
ILIYA LESANI <iliyalesani@gmail.com>
2022-01-26 14:48:14 +0330

    Add OpenGL math library

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+#include <GLM/gtc/type_ptr.hpp>
 
 #define RESX 1280
 #define RESY 720
